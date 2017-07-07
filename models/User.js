4b71a774ffce6fd56717f842c7242c1d3f92389b
ByoungJin <kbj2043@gmail.com>
2017-07-07 21:04:04 +0900

    리펙토링 User 비즈니스 로직 모델로 이전

--- models/User.js
+++ models/User.js
@@ -4,10 +4,10 @@ var mongoose = require('mongoose');
 //var planet = require('./Planet');
 
 var schemaOptions = {
-  timestamps: true,
-  toJSON: {
-    virtuals: true
-  }
+    timestamps: true,
+    toJSON: {
+        virtuals: true
+    }
 };
 
 var userSchema;
@@ -26,86 +26,94 @@ userSchema = new mongoose.Schema({
     google: String,
     github: String,
     vk: String,
-    point : {
-        type: { type: String, default:'Point' }, coordinates: [Number],
+    point: {
+        type: {type: String, default: 'Point'}, coordinates: [Number],
     },
-    planet : {
-        name : String,
-        address : String,
+    planet: {
+        name: String,
+        address: String,
 
-        description : String,
-        startDay : Date,
-        endDay : Date,
-        products : [{type : mongoose.SchemaTypes.ObjectId, ref : 'Product'}],
-        comments : [{
-            userId : {type : mongoose.SchemaTypes.ObjectId, ref : 'User'},
-            contents : String,
-            createDay : String
+        description: String,
+        startDay: Date,
+        endDay: Date,
+        products: [{type: mongoose.SchemaTypes.ObjectId, ref: 'Product'}],
+        comments: [{
+            userId: {type: mongoose.SchemaTypes.ObjectId, ref: 'User'},
+            contents: String,
+            createDay: String
         }]
     },
-    favoritePlanet : [{type : mongoose.SchemaTypes.ObjectId, ref : 'User'}],
-    favoriteProducts : [{type : mongoose.SchemaTypes.ObjectId, ref : 'Product'}],
+    favoritePlanet: [{type: mongoose.SchemaTypes.ObjectId, ref: 'User'}],
+    favoriteProducts: [{type: mongoose.SchemaTypes.ObjectId, ref: 'Product'}],
 
 }, schemaOptions).plugin(require('mongoose-id'));
-userSchema.index({ point: '2dsphere' });
+userSchema.index({point: '2dsphere'});
 
-userSchema.pre('save', function(next) {
-  var user = this;
-  if (!user.isModified('password')) { return next(); }
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(user.password, salt, null, function(err, hash) {
-      user.password = hash;
-      next();
+userSchema.pre('save', function (next) {
+    var user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(user.password, salt, null, function (err, hash) {
+            user.password = hash;
+            next();
+        });
     });
-  });
 });
 
-userSchema.methods.comparePassword = function(password, cb) {
-  bcrypt.compare(password, this.password, function(err, isMatch) {
-    cb(err, isMatch);
-  });
+userSchema.methods.comparePassword = function (password, cb) {
+    bcrypt.compare(password, this.password, function (err, isMatch) {
+        cb(err, isMatch);
+    });
 };
 
-userSchema.virtual('gravatar').get(function() {
-  if (!this.get('email')) {
-    return 'https://gravatar.com/avatar/?s=200&d=retro';
-  }
-  var md5 = crypto.createHash('md5').update(this.get('email')).digest('hex');
-  return 'https://gravatar.com/avatar/' + md5 + '?s=200&d=retro';
+userSchema.virtual('gravatar').get(function () {
+    if (!this.get('email')) {
+        return 'https://gravatar.com/avatar/?s=200&d=retro';
+    }
+    var md5 = crypto.createHash('md5').update(this.get('email')).digest('hex');
+    return 'https://gravatar.com/avatar/' + md5 + '?s=200&d=retro';
 });
 
 userSchema.options.toJSON = {
-  transform: function(doc, ret, options) {
-    delete ret.password;
-    delete ret.passwordResetToken;
-    delete ret.passwordResetExpires;
-  }
+    transform: function (doc, ret, options) {
+        delete ret.password;
+        delete ret.passwordResetToken;
+        delete ret.passwordResetExpires;
+    }
 };
 
 // 비지니스 로직
 userSchema.statics = {
-  list : function (_id, longitude, latitude) {
-      return this.find({$and: [
-        {
-            "point" : {
-                $near: {
-                    $geometry: {
-                        type: "Point",
-                        coordinates: [longitude, latitude]
+    list: function (_id, longitude, latitude) {
+        return this.find({
+            $and: [
+                {
+                    "point": {
+                        $near: {
+                            $geometry: {
+                                type: "Point",
+                                coordinates: [longitude, latitude]
+                            }
+                        }
                     }
+                }, {
+                    "_id": {$ne: _id}
                 }
-            }
-        },{
-            "_id": { $ne: _id }
-        }
-    ]},{
-        name : true,
-        email : true,
-        point : true,
-        planet : true
-    }).limit(5).populate('planet.products').exec();
-    // return users;
-  }
+            ]
+        }, {
+            name: true,
+            email: true,
+            point: true,
+            planet: true
+        }).limit(5).populate('planet.products').exec();
+        // return users;
+    },
+    getUser: function (email) {
+        return this.findOne({email: email}).populate('planet.products').exec();
+    }
+
 
 };
 
